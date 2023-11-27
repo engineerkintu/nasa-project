2c45756dc8ecb8e2623fbfa8a9a73ae9565b86e6
Moses Kintu <engineerkintu@gmail.com>
2023-11-27 14:46:20 +0300

    gitignore

---
+++ server/src/services/query.js
@@ -0,0 +1,14 @@
+const DEFUALT_PAGE_NUMBER = 1
+const DEFUALT_PAGE_LIMIT = 0;
+function getPagination(query){
+    const page = Math.abs(query.page) || DEFUALT_PAGE_NUMBER;
+    const limit = Math.abs(query.limit) || DEFUALT_PAGE_LIMIT;
+    const skip = (page - 1) * limit;
+
+    return {
+        skip,
+        limit,
+    };  
+}
+
+module.exports = {getPagination};
